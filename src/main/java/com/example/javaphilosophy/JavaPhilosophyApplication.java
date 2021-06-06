718969ede59b8f8b1cbcbbe286e7de36e5537d54
Svyatoslav Panifidkin <svyatoslavchikviking@gmail.ru>
2021-06-06 19:48:11 +0700

    Initial commit

---
+++ src/main/java/com/example/javaphilosophy/JavaPhilosophyApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaphilosophy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPhilosophyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPhilosophyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaphilosophy/JavaPhilosophyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaphilosophy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPhilosophyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
